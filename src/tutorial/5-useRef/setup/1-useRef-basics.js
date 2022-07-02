12b11bf79c5b762ac54ba65fa54e50f2ae21e144
Folareen <sakawahab03@gmail.com>
2022-07-02 15:24:40 +0100

    useRef

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,51 @@
-import React, { useEffect, useRef } from 'react';
+import React, {useState, useEffect, useRef } from 'react';
 
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
 const UseRefBasics = () => {
-  return <h2>useRef</h2>;
+  const refContainer = useRef(null);
+  const [names, setNames] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setNames([...names, refContainer.current.value]);
+    refContainer.current.value = ""
+  };
+
+  useEffect(() => {
+    // console.log(refContainer.current);
+    refContainer.current.focus();
+    // console.log(articleContainer.current.innerText)
+  });
+
+  return (
+    <>
+      <form className='form' onSubmit={handleSubmit}>
+        <div>
+          <input type='text' ref={refContainer} />
+        </div>
+        <button type='submit'>submit</button>
+      </form>
+      {
+        names ?
+        names.map(
+          (item, index) => {
+            return <div className='item' key={index}>
+              {item}
+            </div>
+          }
+        )
+        : 
+        (
+          <div className='item'>
+            No item to show..
+          </div>
+        )
+      }
+    </>
+  );
 };
 
 export default UseRefBasics;
