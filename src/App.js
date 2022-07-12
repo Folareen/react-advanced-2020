98a383536d8e7d227b14b8dd74fac109ff52a245
Folareen <sakawahab03@gmail.com>
2022-07-12 01:13:09 +0100

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Setup from './tutorial/5-useRef/setup/1-useRef-basics';
+import React from "react";
+import Setup from "./tutorial/6-useReducer/setup/index";
 
 function App() {
   return (
-    <div className='container'>
+    <div className="container">
       <Setup />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,72 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from "react";
+import Modal from "./Modal";
+import { data } from "../../../data";
 // reducer function
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_PERSON":
+      return {
+        ...state,
+        isModalOpen: true,
+        people: [...state.people, action.payload],
+        modalContent: "Person added",
+      };
+    case "EMPTY":
+      return {
+        ...state,
+        isModalOpen: true,
+        modalContent: "Pls enter a value",
+      };
+  }
+};
+
+const defaultState = {
+  people: [],
+  isModalOpen: false,
+  ModalContent: "",
+};
+
 const Index = () => {
-  return <h2>useReducer</h2>;
+  const [state, dispatch] = useReducer(reducer, defaultState);
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newPerson = { id: new Date().getTime().toString(), name };
+    if (name) {
+      dispatch({ type: "ADD_PERSON", payload: newPerson });
+      setName("");
+    } else {
+      dispatch({ type: "EMPTY" });
+    }
+  };
+
+  return (
+    <>
+      {state.isModalOpen && <Modal modalContent={state.modalContent} />}
+      <form onSubmit={handleSubmit}>
+        <div className="form">
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="btn">
+            Add
+          </button>
+        </div>
+      </form>
+      {state.people &&
+        state.people.map(({ name, id }) => {
+          return (
+            <div key={id}>
+              <h4>{name}</h4>
+            </div>
+          );
+        })}
+    </>
+  );
 };
 
 export default Index;
